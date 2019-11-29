8962109ff07b4f5d345fc30d59060d4e6d684ab3
root <root@localhost.localdomain>
2019-11-29 17:09:54 -0500

    add send and recive method in thread

--- Client.py
+++ Client.py
@@ -1,22 +1,35 @@
 import socket
+import threading
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 host = input("IP of server : ")
 port = input("port of server : ")
+name = input("your name : ")
+
 s.connect((str(host),int(port)))
-s.send("Amin connected".encode())
-#s.close();
+s.send((name + " connected").encode())
+
 messag=""
-def ts(str):
-   str='amin > ' + messag
-   s.send(str.encode()) 
-   data = ''
-   #data = s.recv(1024).decode()
-while 2:
-   messag = input()
-   #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   #host ="localhost"
-   #port =8000
-   #s.connect((host,port))
-   ts(s)
-   #s.close ()
+
+def send(st):
+    while True:
+        messag = input(name + " > ")
+        str = name + ' > ' + messag
+        st.send(str.encode())
+
+def receive(st):
+    while True:
+        data = st.recv(1024).dicode()
+        print(data)
+
+sendThread = threading.Thread(target = send, args = (s,))
+receiveThread = threading.Thread(target = receive, args = (s,))
+
+sendThread.start()
+receiveThread.start()
+
+while True:
+    pass
+
 s.close()

e69b8a1eb1618536698a211a88177e48e0749cf0
AM.A <janipooraminallah@gmail.com>
2019-11-30 01:44:11 +0330

    add recv method in thread

--- Server.py
+++ Server.py
@@ -1,4 +1,20 @@
 import socket
+import signal
+import sys
+import threading
+
+def signal_handler(sig, frame):
+    serversocket.close()
+    print('You pressed Ctrl+C!')
+
+    # sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def listen(clientsocket):
+    while True:
+        data = clientsocket.recv(1024).decode()
+        print (data)
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = input("Inter host IP : ")
@@ -11,13 +27,17 @@ serversocket.bind((str(host), int(port)))
 serversocket.listen(5)
 print ('server started and listening')
 
-(clientsocket, address) = serversocket.accept()
+# (clientsocket, address) = serversocket.accept()
+tr = []
 
 while 1:
     (clientsocket, address) = serversocket.accept()
     #print ("connection found!")
-    data = clientsocket.recv(1024).decode()
-    print (data)
-    print (clientsocket.type)
+    # data = clientsocket.recv(1024).decode()
+    processThread = threading.Thread(target = listen, args = (clientsocket,))
+    tr.append(processThread)
+    processThread.start()
+    # print (data)
+    # print (clientsocket.type)
     #r='REceieve'
     #clientsocket.send(r.encode())

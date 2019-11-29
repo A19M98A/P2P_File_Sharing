47274dd8263a78123cb80d95b19e276277188c15
AM.A <janipooraminallah@gmail.com>
2019-11-30 00:07:52 +0330

    first test of client

--- Client.py
+++ Client.py
@@ -1,1 +1,22 @@
-print('hello Word')
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = input("IP of server : ")
+port = input("port of server : ")
+s.connect((str(host),int(port)))
+s.send("Amin connected".encode())
+#s.close();
+messag=""
+def ts(str):
+   str='amin > ' + messag
+   s.send(str.encode()) 
+   data = ''
+   #data = s.recv(1024).decode()
+while 2:
+   messag = input()
+   #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   #host ="localhost"
+   #port =8000
+   #s.connect((host,port))
+   ts(s)
+   #s.close ()
+s.close()

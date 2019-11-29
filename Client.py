b70bb06c5a3696a36c851ae5e1148da6ef06c4d2
AM.A <janipooraminallah@gmail.com>
2019-11-30 01:51:26 +0330

    test client

--- Client.py
+++ Client.py
@@ -3,11 +3,11 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = input("IP of server : ")
 port = input("port of server : ")
 s.connect((str(host),int(port)))
-s.send("Amin connected".encode())
+s.send("Ali connected".encode())
 #s.close();
 messag=""
 def ts(str):
-   str='amin > ' + messag
+   str='Ali > ' + messag
    s.send(str.encode()) 
    data = ''
    #data = s.recv(1024).decode()
